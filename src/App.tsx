15fb7c449253595195cd69d2111842c085785495
Nadia Jamhari <62807626+nadiajamhari@users.noreply.github.com>
2023-05-22 20:51:13 +0800

    [Add] background

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import "./App.css";
+import AqiqahCard from "./modules/aqiqah-card/AqiqahCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header bg-red-500">
-        <p className="">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AqiqahCard />
     </div>
   );
 }
---
+++ src/modules/aqiqah-card/AqiqahCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./aqiqahcard.css";
+
+const AqiqahCard = () => {
+  return <div className="aqiqah-card-container">Hello world</div>;
+};
+
+export default AqiqahCard;
